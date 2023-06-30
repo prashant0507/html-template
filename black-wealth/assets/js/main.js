0651bcad6bac81383e880a8c6cce0e59a3d648d5
pra98935 <pra98935@gmail.com>
2023-06-30 16:23:44 +0530

    video section

--- black-wealth/assets/js/main.js
+++ black-wealth/assets/js/main.js
@@ -1,13 +1,19 @@
-console.log("jojoojo");
-
 const activateTab = (e, id) => {
-    console.log(e);
+  document.getElementById("tab1").classList.remove("active");
+  document.getElementById("tab2").classList.remove("active");
+  document.getElementById("tab-content1").classList.remove("active");
+  document.getElementById("tab-content2").classList.remove("active");
 
-    document.getElementById("tab1").classList.remove("active");
-    document.getElementById("tab2").classList.remove("active");
-    document.getElementById("tab-content1").classList.remove("active");
-    document.getElementById("tab-content2").classList.remove("active");
-    
-    e.target.classList.add("active");
-    document.getElementById(id).classList.add("active");
-} 
+  e.target.classList.add("active");
+  document.getElementById(id).classList.add("active");
+};
+
+const activeVideo = (e) => {
+  const btnContainer = document.getElementById("vr");
+  const btns = btnContainer.getElementsByClassName("active");
+  for (var i = 0; i < btns.length; i++) {
+    const current = btnContainer.getElementsByClassName("active");
+    current[0].className = current[0].className.replace("active", "");
+  }
+  e.currentTarget.classList.add("active");
+};
